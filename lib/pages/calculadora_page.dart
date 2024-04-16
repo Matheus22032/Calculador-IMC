80204169951e94400cf39b3f737de7575912f35a
Matheus Gonçalves <matheus.g2003@hotmail.com>
2024-04-16 11:50:00 -0300

    initial commit

---
+++ lib/pages/calculadora_page.dart
@@ -0,0 +1,104 @@
+import '../components/bottom_button.dart';
+import 'package:calculadora_imc_24/components/contador.dart';
+import 'package:calculadora_imc_24/components/custom_card.dart';
+import 'package:calculadora_imc_24/components/gender_content.dart';
+import 'package:calculadora_imc_24/components/modal_result.dart';
+
+import 'package:calculadora_imc_24/components/slider_altura.dart';
+
+import 'package:flutter/material.dart';
+
+enum Sexo { masculino, feminino }
+
+class CalculadoraPage extends StatefulWidget {
+  const CalculadoraPage({super.key});
+
+  @override
+  State<CalculadoraPage> createState() => _CalculadoraPageState();
+}
+
+class _CalculadoraPageState extends State<CalculadoraPage> {
+  Sexo? genderSelected;
+  int altura = 120;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Calculadora IMC'),
+        centerTitle: true,
+      ),
+      body:  Column(
+        children: [
+          Expanded(
+            child: Row(
+              children: [
+                Expanded(
+                  child: GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        genderSelected = Sexo.masculino;
+                      });
+                    } ,
+                    child: CustomCard(
+                      active: genderSelected == Sexo.masculino,
+                      child: GenderContent(
+                        icon: Icons.male,
+                        label: 'Masculino',
+                      ),
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        genderSelected = Sexo.feminino;
+                      });
+                    },
+                    child: CustomCard(
+                      active: genderSelected == Sexo.feminino,
+                      child: GenderContent(
+                        icon: Icons.female,
+                        label: 'Feminino',
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: CustomCard(
+              child: SliderAltura(
+                onChanged: (double value){
+                  setState(() {
+                    altura =value.toInt();
+                  });
+                
+                },
+                value: altura,
+              ),
+            ),
+          ),
+          Expanded(
+            child: Row(
+              children: [
+                Expanded(
+                  child: CustomCard(
+                    child: Contador(),
+                  ),
+                ),
+                Expanded(
+                  child: CustomCard(
+                    child: Contador(),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          BottomButton(buttonTitle: 'Calcular IMC',)
+        ],
+      ),
+    );
+  }
+}
